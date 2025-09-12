a7290a5649fd851df5ad19b08953605b0a6cb551
Babay <babayvolohatyu@gmail.com>
2025-09-12 15:27:37 +0300

    Add Login controller

---
+++ LinkShortener/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using LinkShortener.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkShortener.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserRepository _repository;
+
+        public LoginController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
