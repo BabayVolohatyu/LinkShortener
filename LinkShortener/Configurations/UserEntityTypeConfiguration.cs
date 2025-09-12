485fb8bbc35cae56dc5b542f87a7632439bf2982
Babay <babayvolohatyu@gmail.com>
2025-09-12 14:29:54 +0300

    Add user model configuration

---
+++ LinkShortener/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using LinkShortener.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LinkShortener.Configurations
+{
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasKey(u => u.Id);
+
+            builder
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
--- LinkShortener/Data/LinkShortenerDbContext.cs
+++ LinkShortener/Data/LinkShortenerDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LinkShortener.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LinkShortener.Data
 {
     public class LinkShortenerDbContext : DbContext
     {
         public LinkShortenerDbContext(DbContextOptions<LinkShortenerDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LinkShortenerDbContext).Assembly);
+        }
     }
 }
