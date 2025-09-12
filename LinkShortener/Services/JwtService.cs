bae3c2d118b268d7ec1e20bab1d3eccbd0064bc0
Babay <babayvolohatyu@gmail.com>
2025-09-12 20:04:29 +0300

    Add jwt service

---
+++ LinkShortener/Services/JwtService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LinkShortener.Services
+{
+    public class JwtService
+    {
+        private readonly string secureKey;
+        public JwtService(IConfiguration configuration)
+        {
+            secureKey = configuration["JwtSettings:SecretKey"]
+                    ?? throw new ArgumentNullException("Jwt key doesn't exist");
+        }
+        public string Generate(int? id = null, string? role = null)
+        {
+            var claims = new List<Claim>();
+
+            if (id.HasValue)
+                claims.Add(new Claim("id", id.Value.ToString()));
+
+            if (!string.IsNullOrEmpty(role))
+                claims.Add(new Claim(ClaimTypes.Role, role));
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var header = new JwtHeader(credentials);
+
+            var payload = new JwtPayload(
+                null,
+                null,
+                claims,
+                null,
+                DateTime.UtcNow.AddHours(1));
+
+            var securityToken = new JwtSecurityToken(header, payload);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
