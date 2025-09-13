ca892deef94400697d437bbd66c498973b827f47
Babay <babayvolohatyu@gmail.com>
2025-09-13 08:22:17 +0300

    Add url model

---
+++ LinkShortener/Models/Url.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkShortener.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortUrl { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.Now;
+        public string Description {  get; set; } = string.Empty;
+
+    }
+}
