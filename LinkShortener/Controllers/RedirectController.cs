7f5b8adb5c76c2545c279d4851e6c620b16ba79f
Babay <babayvolohatyu@gmail.com>
2025-09-13 16:34:59 +0300

    Add Redirect controller

---
+++ LinkShortener/Controllers/RedirectController.cs
@@ -0,0 +1,26 @@
+﻿using LinkShortener.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("")]
+public class RedirectController : Controller
+{
+    private readonly IUrlRepository _urlRepository;
+
+    public RedirectController(IUrlRepository urlRepository)
+    {
+        _urlRepository = urlRepository;
+    }
+
+    [HttpGet("{code}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> RedirectToOriginal(string code)
+    {
+        var url = await _urlRepository.GetByCodeAsync(code);
+        if (url == null)
+            return NotFound();
+
+        return Redirect(url.OriginalUrl);
+    }
+}
+
