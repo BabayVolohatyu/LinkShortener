831bea62812559bd602d8d95314df00f12284c4e
Babay <babayvolohatyu@gmail.com>
2025-09-13 13:28:59 +0300

    Expand app

--- LinkShortener/Client/src/app/app.routes.ts
+++ LinkShortener/Client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { IndexComponent } from './pages/index/index.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  {path: 'index', component: IndexComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
