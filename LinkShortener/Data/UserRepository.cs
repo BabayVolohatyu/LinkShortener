868ea0d8cb8b72cdfc523c32ab69b2d6a6482884
Babay <babayvolohatyu@gmail.com>
2025-09-13 09:32:18 +0300

    Add GetByIdAsync

--- LinkShortener/Data/UserRepository.cs
+++ LinkShortener/Data/UserRepository.cs
@@ -12,11 +12,17 @@ namespace LinkShortener.Data
         {
             _context = context;
         }
+
         public async Task<User> CreateAsync(User user) 
         {
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+
+        public async Task<User?> GetByIdAsync(int id) 
+        {
+            return await _context.Users.FindAsync(id);
         }
 
         public async Task<User?> GetByEmailAsync(string email) 
