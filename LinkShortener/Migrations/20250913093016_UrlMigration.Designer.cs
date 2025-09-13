98f4e6e719cbb55d4db74d040257b6aac84d7ae0
Babay <babayvolohatyu@gmail.com>
2025-09-13 13:30:00 +0300

    Add url migration

--- LinkShortener/Migrations/20250913093016_UrlMigration.Designer.cs
+++ LinkShortener/Migrations/20250913093016_UrlMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LinkShortener.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LinkShortener.Migrations
 {
     [DbContext(typeof(LinkShortenerDbContext))]
-    partial class LinkShortenerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250913093016_UrlMigration")]
+    partial class UrlMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LinkShortener/Migrations/20250913093016_UrlMigration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LinkShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class UrlMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OriginalUrl = table.Column<string>(type: "text", nullable: false),
+                    Code = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    ShortUrl = table.Column<string>(type: "text", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_Code",
+                table: "Urls",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_OriginalUrl",
+                table: "Urls",
+                column: "OriginalUrl",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_ShortUrl",
+                table: "Urls",
+                column: "ShortUrl",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
