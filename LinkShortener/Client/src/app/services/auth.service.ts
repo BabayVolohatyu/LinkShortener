383019e2cf4aeb525cc5c521bdf0938f111fbf7b
Babay <babayvolohatyu@gmail.com>
2025-09-13 13:26:56 +0300

    Add services

---
+++ LinkShortener/Client/src/app/services/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private tokenKey = 'auth_token';
+  private jwtHelper = new JwtHelperService();
+
+  constructor(private router: Router) { }
+
+  // Save token after login
+  setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  clearToken(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    const token = this.getToken();
+    return token != null && !this.jwtHelper.isTokenExpired(token);
+  }
+
+  getDecodedToken(): any {
+    const token = this.getToken();
+    if (!token) return null;
+    return this.jwtHelper.decodeToken(token);
+  }
+
+  getUserId(): number | null {
+    const decoded = this.getDecodedToken();
+    return decoded?.id ? Number(decoded.id) : null;
+  }
+
+  getUserName(): string {
+  return this.getClaim('unique_name') || this.getClaim('name') || "Unknown";
+  }
+
+
+
+  hasRole(role: string): boolean {
+    const decoded = this.getDecodedToken();
+    if (!decoded || !decoded.role) return false;
+
+    if (Array.isArray(decoded.role)) {
+      return decoded.role.includes(role);
+    }
+    return decoded.role === role;
+  }
+
+  getClaim(key: string): string | null {
+  const decoded = this.getDecodedToken();
+  return decoded?.[key] || null;
+  }
+
+}
---
+++ LinkShortener/Client/src/app/services/url.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UrlService {
+  private apiUrl = `${environment.apiUrl}/index`;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl, { withCredentials: true });
+  }
+
+  create(originalUrl: string): Observable<any> {
+    return this.http.post<any>(
+      this.apiUrl,
+      { originalUrl },
+      { withCredentials: true }
+    );
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.post<void>(
+      `${this.apiUrl}/${id}`,
+      {},
+      { withCredentials: true }
+    );
+  }
+}
