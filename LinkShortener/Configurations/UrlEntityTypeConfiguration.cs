b77b1206716c0a0ad017c091fbb58f2dbf295cdb
Babay <babayvolohatyu@gmail.com>
2025-09-13 08:39:02 +0300

    Add url config

---
+++ LinkShortener/Configurations/UrlEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using LinkShortener.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LinkShortener.Configurations
+{
+    public class UrlEntityTypeConfiguration : IEntityTypeConfiguration<Url>
+    {
+        public void Configure(EntityTypeBuilder<Url> builder) 
+        {
+            builder
+                .HasKey(u => u.Id);
+
+            builder
+                .HasIndex(u => u.OriginalUrl)
+                .IsUnique();
+
+            builder
+                .HasIndex(u => u.ShortUrl)
+                .IsUnique();
+        }
+    }
+}
