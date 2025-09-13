3577eeeebec99a51311f943061758523f93445d8
Babay <babayvolohatyu@gmail.com>
2025-09-13 17:35:57 +0300

    Add some info to test

--- LinkShortener/Data/LinkShortenerDbContext.cs
+++ LinkShortener/Data/LinkShortenerDbContext.cs
@@ -10,9 +10,31 @@ namespace LinkShortener.Data
         public DbSet<User> Users { get; set; }
 
         public DbSet<Url> Urls { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(LinkShortenerDbContext).Assembly);
+
+
+
+            //for example only
+            modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Id = 5555,
+                Name = "Test Admin",
+                Email = "admin@test.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("0000"),   
+                Role = "Admin"
+            },
+            new User
+            {
+                Id = 6666,
+                Name = "Test User",
+                Email = "user@test.com",
+                Password = BCrypt.Net.BCrypt.HashPassword("0000"),  
+                Role = "User"
+            }
+        );
         }
     }
 }
