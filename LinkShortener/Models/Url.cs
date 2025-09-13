94251e8fdada1959c9e670b06bcfe58ba54ff5df
Babay <babayvolohatyu@gmail.com>
2025-09-13 10:27:04 +0300

    Add code property to the url model

--- LinkShortener/Models/Url.cs
+++ LinkShortener/Models/Url.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string OriginalUrl { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
         public string ShortUrl { get; set; } = string.Empty;
         public string CreatedBy { get; set; } = string.Empty;
         public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.Now;
