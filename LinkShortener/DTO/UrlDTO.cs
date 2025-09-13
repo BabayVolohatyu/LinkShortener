5bfb9d9d46fe15a5cab6edf7ae9fc6171d07bf40
Babay <babayvolohatyu@gmail.com>
2025-09-13 08:22:34 +0300

    Add url dto

---
+++ LinkShortener/DTO/UrlDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkShortener.DTO
+{
+    public class UrlDTO
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string ShortUrl { get; set; } = string.Empty;
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTimeOffset CreatedDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
